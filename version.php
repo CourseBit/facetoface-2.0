abdf7974f08d09e0f0328be731faac65ba332850
Joseph Conradt <joseph.conradt@coursebit.net>
2017-06-07 13:54:36 -0500

    Change highlight color

--- version.php
+++ version.php
@@ -31,7 +31,7 @@
 defined('MOODLE_INTERNAL') || die();
 
 $plugin->component = 'mod_facetoface';
-$plugin->version   = 2017052400;
+$plugin->version   = 2017060700;
 $plugin->requires  = 2016120500;
 $plugin->release   = '3.2.0 (Build: 2017052400)';
 $plugin->maturity  = MATURITY_STABLE;
